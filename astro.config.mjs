031e6031320fee373385e040b91b29df2f8b4f33
Olcan EBREM <olcanebrem@gmail.com>
2024-07-17 01:51:19 +0300

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
-import netlify from "@astrojs/netlify";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
