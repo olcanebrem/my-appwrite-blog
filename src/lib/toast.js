8104e19b504e4ae77c65d8fc917526bfbbcefd44
Olcan EBREM <olcanebrem@gmail.com>
2024-07-28 03:24:18 +0300

    toast oto close

--- src/lib/toast.js
+++ src/lib/toast.js
@@ -1,28 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
   const toastElement = document.getElementById('toast');
-  toastElement.classList.add('show');
 
-  // 2 saniye sonra toast bildiriminin kaybolmasını sağlayan zamanlayıcı
-  setTimeout(() => {
-    toastElement.classList.add('hide');
-    toastElement.addEventListener('animationend', () => {
-      toastElement.style.display = 'none';
-    });
-  }, 2000);
+  // Toast daha önce gösterildiyse localStorage'da 'toastShown' anahtarına bak
+  const isToastShown = localStorage.getItem('toastShown');
 
-  // "Done" düğmesine tıklanırsa toast bildirimi kaybolur
-  document.querySelector('.btn-done').addEventListener('click', function() {
-    toastElement.classList.add('hide');
-    toastElement.addEventListener('animationend', () => {
-      toastElement.style.display = 'none';
-    });
-  });
+  if (!isToastShown) {
+    // Toast daha önce gösterilmediyse göster ve 'toastShown' anahtarını ayarla
+    toastElement.classList.add('show');
 
-  // Kapatma simgesine tıklanırsa toast bildirimi kaybolur
-  document.querySelector('.btn-close').addEventListener('click', function() {
-    toastElement.classList.add('hide');
-    toastElement.addEventListener('animationend', () => {
-      toastElement.style.display = 'none';
+    // 2 saniye sonra toast bildiriminin kaybolmasını sağlayan zamanlayıcı
+    setTimeout(() => {
+      toastElement.classList.add('hide');
+      toastElement.addEventListener('animationend', () => {
+        toastElement.style.display = 'none';
+      });
+      // Toast'ın gösterildiğini kaydet
+      localStorage.setItem('toastShown', 'true');
+    }, 3000);
+
+    // "Done" düğmesine tıklanırsa toast bildirimi kaybolur
+    document.querySelector('.btn-done').addEventListener('click', function() {
+      toastElement.classList.add('hide');
+      toastElement.addEventListener('animationend', () => {
+        toastElement.style.display = 'none';
+      });
+      // Toast'ın gösterildiğini kaydet
+      localStorage.setItem('toastShown', 'true');
     });
-  });
+
+    // Kapatma simgesine tıklanırsa toast bildirimi kaybolur
+    document.querySelector('.btn-close').addEventListener('click', function() {
+      toastElement.classList.add('hide');
+      toastElement.addEventListener('animationend', () => {
+        toastElement.style.display = 'none';
+      });
+      // Toast'ın gösterildiğini kaydet
+      localStorage.setItem('toastShown', 'true');
+    });
+  }
 });
