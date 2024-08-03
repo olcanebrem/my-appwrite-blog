33c19972be23fbddc5650b03b602cd74fd763603
Olcan EBREM <olcanebrem@gmail.com>
2024-08-04 00:38:59 +0300

    fdg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -8,27 +7,20 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nomy.vercel.app",
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "github-dark-dimmed",
-      },
-      gfm: true,
-    }),
-    icon(),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    simpleStackForm(),
-  ],
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed"
+    },
+    gfm: true
+  }), icon(), sitemap(), react(), tailwind({
+    applyBaseStyles: false
+  }), simpleStackForm()],
   output: "hybrid",
-  adapter: vercel({
-    analytics: true,
-  }),
+  adapter: netlify()
 });
